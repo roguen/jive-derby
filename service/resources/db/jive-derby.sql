e71becf0e3447f586bd492874158bbafdc7247db
Roguen Keller <roguen@gmail.com>
2019-09-22 04:24:52 +0000

    altered race results table to add car id

--- service/resources/db/jive-derby.sql
+++ service/resources/db/jive-derby.sql
@@ -69,6 +69,7 @@ CREATE TABLE jderby_results (
   totalTimeSec NUMERIC,
   speed NUMERIC,
   isPrimary BOOLEAN DEFAULT false,
+  carID INTEGER REFERENCES jderby_reg_cars (carID),
   isActive BOOLEAN
 );
 ALTER SEQUENCE jderby_results_id_seq OWNED BY jderby_results.resultID;
@@ -123,16 +124,16 @@ CREATE SEQUENCE jderby_reg_cars_id_seq;
 CREATE TABLE jderby_reg_cars
 (
     carID INTEGER PRIMARY KEY DEFAULT nextval('jderby_reg_cars_id_seq'),
-	regID INTEGER REFERENCES jderby_reg_racers (id),
+    regID INTEGER REFERENCES jderby_reg_racers (id),
     weight NUMERIC,
     reactUUID VARCHAR(255),
-	frontPicURL VARCHAR(255),
-	rightPicURL VARCHAR(255),
-	topPicURL VARCHAR(255),
-	anglePicURL VARCHAR(255),
-	backPicURL VARCHAR(255),
-	leftPicURL VARCHAR(255),
-	bottomPicURL VARCHAR(255)
+    frontPicURL VARCHAR(255),
+    rightPicURL VARCHAR(255),
+    topPicURL VARCHAR(255),
+    anglePicURL VARCHAR(255),
+    backPicURL VARCHAR(255),
+    leftPicURL VARCHAR(255),
+    bottomPicURL VARCHAR(255)
 );
 ALTER SEQUENCE jderby_reg_cars_id_seq OWNED BY jderby_reg_cars.carid;
 CREATE INDEX ON jderby_reg_cars (carID);
